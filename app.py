869d79ed529f9802985d9ef6bcbb119e13fb2d97
ninapolekhina <nina.life.researcher@gmail.com>
2023-08-13 18:16:47 +0300

    filters

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, abort, request
+from flask import Flask, abort, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 from flask_migrate import Migrate
 from pathlib import Path
 
 
 BASE_DIR = Path(__file__).parent
+
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -18,20 +20,25 @@ migrate = Migrate(app, db)
 
 class AuthorModel(db.Model):
    id = db.Column(db.Integer, primary_key=True)
-   name = db.Column(db.String(32), unique=True)
-   quotes = db.relationship('QuoteModel', backref='author', lazy='dynamic')
+   name = db.Column(db.String(32))
+   surname = db.Column(db.String(32), nullable=True)
+   __table_args__ = (db.UniqueConstraint('name', 'surname', name='fullname'),)
+   quotes = db.relationship('QuoteModel', backref='author', lazy='dynamic', cascade="all, delete-orphan")
 
    # QuoteModel - связываемся с этой табл,
    # backref - обратная связь: у автора есть цитаты, а у цитаты будет автор
    # lazy - параметр, кот управляет запросами более чем в 1 табл (join)
 
-   def __init__(self, name):
+   def __init__(self, name, surname=None):
        self.name = name
+       self.surname = surname
+
 
    def to_dict(self):
        return {
            "id": self.id,
-           "name": self.name
+           "name": self.name,
+           "surname": self.surname
        }
 
 
@@ -39,38 +46,41 @@ class QuoteModel(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    author_id = db.Column(db.Integer, db.ForeignKey(AuthorModel.id))
    text = db.Column(db.String(255), unique=False)
+   created = db.Column(db.DateTime(timezone=True), server_default=func.now())
+   rating = db.Column(db.Integer, nullable=False, default='1', server_default='1')
 
-   def __init__(self, author, text):
+   def __init__(self, author, text, created=func.now(), rating=1):
        self.author_id = author.id
        self.text = text
+       self.created = created
+       self.rating = rating
 
    def to_dict(self):
        return {
            "id": self.id,
            "name": self.text,
-           "author": self.author.to_dict() #связанный параметр
+           "author": self.author.to_dict(), #связанный параметр
+           "rate": self.rating,
+           "created": self.created
        }
-
 
 
 @app.errorhandler(404)
 def handler_bad_request(error):
-    return "A quote with such parameters was not found", 404
+    return "A quote or author with such parameters was not found", 404
 
 
 #Author
 #--------------------------------------------------------------------------
-#Создаем нового автора
-@app.post("/authors")
-def create_author():
-    author_data = request.json
-    author = AuthorModel(author_data["name"])
-    db.session.add(author)
-    db.session.commit()
-    return author.to_dict(), 201
+#Author. Get by id
+@app.route("/authors/<int:author_id>")
+def get_author_by_id(author_id):
+    author = AuthorModel.query.get(author_id)
+    if author is None:
+        abort(404)
+    return author.to_dict()
 
-
-#Получаем всех авторов
+#Author. Get all
 @app.route("/authors")
 def get_authors():
     authors: list[AuthorModel] = AuthorModel.query.all()
@@ -79,21 +89,54 @@ def get_authors():
         authors_dict.append(author.to_dict())
     return authors_dict
 
-#Удаляем автора
+#Author. Create
+@app.post("/authors")
+def create_author():
+    author_data = request.json
+    name = author_data["name"]
+    author = AuthorModel(name)
+    if len(author_data["surname"]) != 0:
+        surname = author_data["surname"]
+        author = AuthorModel(name, surname)
+    db.session.add(author)
+    db.session.commit()
+    return author.to_dict(), 201
+
+#Author. Edit
+@app.put("/authors/<int:author_id>")
+def edit_author(author_id):
+    author_data = request.json
+    author = AuthorModel.query.get(author_id)
+    if author is None:
+        abort(404)
+    elif len(author_data["name"]) == 0 and len(author_data["surname"]) == 0:
+        return "To edit, enter the first and/or last name of the author."
+    if len(author_data["name"]) != 0:
+        author.name = author_data["name"]
+    if len(author_data["surname"]) != 0:
+        author.surname = author_data["surname"]
+    db.session.add(author)
+    db.session.commit()
+    return jsonify(author.to_dict()), 200
+
+#Author. Delete
 @app.delete("/authors/<int:author_id>")
 def delete_author(author_id):
     author = AuthorModel.query.get(author_id)
     if author is None:
         abort(404)
-    else:
-        db.session.delete(author)
-        db.session.commit()
-        return f"Author with id={author_id} has deleted", 200
+    db.session.delete(author)
+    db.session.commit()
+    return f"Author with id={author_id} has deleted", 200
 
 
+#Quotes
+#-------------------------------------------------------------------------------
 # dict -> JSON - сериализация
 # JSON -> dict - десериализация
 # Object -> dict (функция) -> JSON (фреймворк)
+
+#Quote. Get all quotes
 # http://127.0.0.1:5000/quotes
 @app.route("/quotes")
 def get_quotes():
@@ -103,7 +146,7 @@ def get_quotes():
         quotes_dict.append(quote.to_dict())
     return quotes_dict
 
-#Получаем цитату по id
+#Quote. Get by id
 # http://127.0.0.1:5000/quotes/1
 @app.route("/quotes/<int:quote_id>")  # шаблон урла
 def get_quote_by_id(quote_id):
@@ -112,57 +155,127 @@ def get_quote_by_id(quote_id):
         abort(404)
     return quote.to_dict()
 
-#Создаем цитату
+#Quote. Get all author`s quotes
+# http://127.0.0.1:5000/authors/2/quotes
+@app.route("/authors/<int:author_id>/quotes")
+def get_all_quotes_by_author(author_id):
+    quotes: list[QuoteModel] = QuoteModel.query.filter_by(author_id=f"{author_id}")
+    quotes_dict: list[dict] = []
+    for quote in quotes:
+        quotes_dict.append(quote.to_dict())
+    if len(quotes_dict) == 0:
+        abort(404)
+    return quotes_dict
+
+#Quote. Create
 @app.route("/authors/<int:author_id>/quotes", methods=["POST"])
 def create_quote(author_id):
     author = AuthorModel.query.get(author_id)
     new_quote = request.json
+    if int(new_quote['rating']) > 5:
+        new_quote['rating'] = '5'
+    elif int(new_quote['rating']) <= 0:
+        new_quote['rating'] = '1'
     quote = QuoteModel(author, **new_quote) #распаковка через **
     db.session.add(quote)
     db.session.commit()
     return quote.to_dict(), 201
 
+#Quote. Edit
+@app.put("/quotes/<int:quote_id>")
+def edit_quote(quote_id):
+    new_quote = request.json
+    quote = QuoteModel.query.get(quote_id)
+    if quote is None:
+        abort(404)
+    quote.text = new_quote["text"]
+    db.session.commit()
+    return jsonify(quote.to_dict()), 200
+
+@app.delete("/quotes/<int:quote_id>")
+def delete_quote(quote_id):
+    quote = QuoteModel.query.get(quote_id)
+    if quote is None:
+        abort(404)
+    else:
+        db.session.delete(quote)
+        db.session.commit()
+        return f"Quote with id={quote_id} has deleted", 200
 
 
+#Over requests of filters
+#-------------------------------------------------------------------------------
+@app.get("/quotes/<int:quote_id>/increase_rating")
+def increase_rating(quote_id):
+    quote = QuoteModel.query.get(quote_id)
+    if quote is None:
+        abort(404)
+    if quote.rating < 5:
+        quote.rating += 1
+        db.session.commit()
+        return jsonify(quote.to_dict()), 200
+    return f"Rating for quote {quote_id} is maxed out", 200
+    
+@app.get("/quotes/<int:quote_id>/decrease_rating")
+def decrease_rating(quote_id):
+    quote = QuoteModel.query.get(quote_id)
+    if quote is None:
+        abort(404)
+    if quote.rating > 1:
+        quote.rating -= 1
+        db.session.commit()
+        return jsonify(quote.to_dict()), 200
+    return f"rating for quote {quote_id} is minumum", 200
 
-# @app.route("/quotes/<int:quote_id>", methods=["PUT"])
-# def edit_quote(quote_id):
-#     new_quote = request.json
-#     author = new_quote['author']
-#     text = new_quote['text']
-#     if len(author) == 0 and len(text) == 0:
-#         return "Add a new text or author to edit the quote", 400
-#     else:
-#         conn = get_db()
-#         if len(author) == 0:
-#             sql = f"UPDATE quotes SET text = '{text}' WHERE id = {quote_id};"
-#         elif len(text) == 0:
-#             sql = f"UPDATE quotes SET author = '{author}' WHERE id = {quote_id};"
-#         else:
-#             sql = f"UPDATE quotes SET author = '{author}', text = '{text}' WHERE id = {quote_id};"
-#         cursor = conn.cursor().execute(sql)
-#         conn.commit()
-#         result = cursor.rowcount
-#         cursor.close()
-#         if result == 1:
-#             return new_quote
-#             # return f"Quote with id {quote_id} changed.", 200
-#         else:
-#             return abort(404)
-#
-# @app.route("/quotes/<quote_id>", methods=['DELETE'])
-# def delete(quote_id):
-#     conn = get_db()
-#     sql = f"DELETE FROM quotes WHERE id = {quote_id};"
-#     cursor = conn.cursor().execute(sql)
-#     conn.commit()
-#     result = cursor.rowcount
-#     cursor.close()
-#     if result == 1:
-#         return f"Quote with id {quote_id} is deleted.", 200
-#     else:
+#Получаем всех авторов с именем или с двумя (ПР, Nina)
+# http://127.0.0.1:5000/authors/filters?name=nina
+@app.get("/authors/filters")
+def get_authors_by():
+    name = request.args.get('name', default=None, type=None)
+    name2 = request.args.get('name2', default=None, type=None)
+    surname = request.args.get('surname', default=None, type=None)
+    surname2 = request.args.get('surname2', default=None, type=None)
+    authors = AuthorModel.query.filter((AuthorModel.name.ilike(f"%{name}%")) | (AuthorModel.name.ilike(f"%{name2}%")) | (AuthorModel.surname.ilike(f"%{surname}%")) | (AuthorModel.surname.ilike(f"%{surname2}%")))
+    author_dict: list[dict] = []
+    for author in authors:
+        author_dict.append(author.to_dict())
+    if len(author_dict) == 0:
+        return abort(404)
+    return author_dict
+
+#Получаем все цитаты по имени автора и/или с определенным рейтингом
+# http://127.0.0.1:5000/quotes/filters?name=rick
+# @app.get("/quotes/filters")
+# def get_quotes_with_filters():
+#     name = request.args.get('name', default=None, type=None)
+#     surname = request.args.get('surname', default=None, type=None)
+#     rating = request.args.get('rating', default=None, type=None)
+#     text = request.args.get('text', default=None, type=None)
+#     quotes = QuoteModel.query.filter((QuoteModel.rating==f"{rating}") | (QuoteModel.text.ilike(f"%{text}%"))
+#     #(QuoteModel.author.has(name=f"%{name}%")) | (QuoteModel.author.has(surname=f"{surname}")) |
+#     # quotes = QuoteModel.query.join(QuoteModel.author).filter(QuoteModel.name == 'Tom')
+#     quote_dict: list[dict] = []
+#     for quote in quotes:
+#         quote_dict.append(quote.to_dict())
+#     if len(quote_dict) == 0:
 #         return abort(404)
+#     return quote_dict
 
+
+#Author. Sorted by name or surname
+@app.route("/authors/sortedby/<tag>")
+def get_sorted_authors(tag):
+    authors: list[AuthorModel] = AuthorModel.query.all()
+    authors_dict: list[dict] = []
+    for author in authors:
+        authors_dict.append(author.to_dict())
+       #
+       # не сортировать null/None
+    if tag == "name":
+        authors_dict = sorted(authors_dict, key=lambda x: x['name'])
+    # elif tag == "surname":
+    #     authors_dict = sorted(authors_dict, key=lambda x: x['surname'])
+    return authors_dict
 
 if __name__ == "__main__":
     app.run(debug=True)
--- task1.py
+++ task1.py
@@ -9,12 +9,83 @@ quotes_tuples = [
     (8, 'Alan Kay', 'Легче изобрести будущее, чем предсказать его')
 ]
 
+#
+# keys = ["id", "author", "text"]
+#
+#
+# quotes = []
+# for quote in quotes_tuples:
+#     quotes.append(dict(zip(keys, quote)))
+#
+# print(quotes)
+#
+# name = "Tim"
+# rating = ""
+# text = "програм"
+#
+# name_filter = ""
+# rating_filter = ""
+# text_filter = ""
+# if len(name) == 0:
+#     name_filter = f"QuoteModel.author.has.ilike(f'%{name}%')"
+# if rating is not None:
+#     rating_filter = f"QuoteModel.rating=={rating}"
+# if text is not None:
+#    text_filter = f"QuoteModel.text.ilike(f'%{text}%')"
+# quotes = f"QuoteModel.query.filter({name_filter} + {rating_filter} + {text_filter})"
+#
+# print(quotes)
 
-keys = ["id", "author", "text"]
 
+authors_dict = [
+    {
+        "id": 1,
+        "name": "Stepan",
+        "surname": None
+    },
+    {
+        "id": 2,
+        "name": "nina",
+        "surname": None
+    },
+    {
+        "id": 5,
+        "name": "Rick Cook",
+        "surname": None
+    },
+    {
+        "id": 6,
+        "name": "Alan J. Perlis",
+        "surname": None
+    },
+    {
+        "id": 7,
+        "name": "Waldi Ravens",
+        "surname": None
+    },
+    {
+        "id": 8,
+        "name": "Mosher’s Law of Software Engineering",
+        "surname": None
+    },
+    {
+        "id": 9,
+        "name": "Bill Bryson",
+        "surname": None
+    }
+]
 
-quotes = []
-for quote in quotes_tuples:
-    quotes.append(dict(zip(keys, quote)))
+print(sorted(authors_dict, key=lambda x: x['name']))
 
-print(quotes)
+request = {args: {
+    'name': 'Nina',
+    'name2': 'Stepan',
+    'surname': 'Krylov',
+    'surname5': 'Polekhina',
+    'age': 33}
+}
+
+other_args = set(request.args.keys()) - {'name', 'name2', 'surname', 'surname2'}: #можно настроить другие параметры, оставила для проверки ограничения
+print(re)
+        # if other_args is not None:
+        #     return f"Use a request to view all authors", 40
